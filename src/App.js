95ec5eb164277e52c4f1e0082a7b00853c2e67c7
Pastor-git <maciej.pastor@gmail.com>
2021-07-02 15:46:05 +0200

    next update css

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
     return (
 
       <div id="App" stylesheet={style} >
-        <div id="header"><text id="welcome_text">It's Rainy MemeApp! Yor rain of memes!</text></div>
+        <div id="header"><text id="welcome_text">It's Rainy Meme! Halelujah! Yor rain of memes! JEEE EE EE!</text></div>
         <Router>
           <div id="navbar" >
             <NavLink to="/mainlist"><button class="navbutton" id="main">MainList</button></NavLink><br/>
